e23de62b6398fc2d8b5c517d9b6adc8d90dd840e
eliasvasylenko <eliasvasylenko@gmail.com>
2014-08-26 17:24:57 +0100

    Made PropertySet more efficient in the case of immutable objects.

--- strangeskies-utilities/src/main/java/uk/co/strangeskies/utilities/PropertySet.java
+++ strangeskies-utilities/src/main/java/uk/co/strangeskies/utilities/PropertySet.java
@@ -11,15 +11,19 @@ public class PropertySet<T> {
 	private final Class<T> objectClass;
 	private final T object;
 	private final Set<Function<? super T, Object>> properties;
+	private final Integer hashCode;
 
-	public PropertySet(Class<T> objectClass, T object) {
+	public PropertySet(Class<T> objectClass, T object, boolean immutable) {
 		this.objectClass = objectClass;
 		this.object = object;
 		properties = new HashSet<>();
+
+		hashCode = immutable ? generateHashCode() : null;
 	}
 
 	@SuppressWarnings("unchecked")
-	public PropertySet(Class<T> objectClass, PropertySet<?> propertySet) {
+	public PropertySet(Class<T> objectClass, PropertySet<?> propertySet,
+			boolean immutable) {
 		if (!propertySet.objectClass.isAssignableFrom(objectClass)
 				|| !objectClass.isInstance(propertySet.object))
 			throw new IllegalArgumentException();
@@ -28,10 +32,18 @@ public class PropertySet<T> {
 		this.object = (T) propertySet.object;
 		properties = new HashSet<>(
 				((PropertySet<? super T>) propertySet).properties);
+
+		hashCode = immutable ? generateHashCode() : null;
 	}
 
 	public PropertySet<T> add(Function<? super T, Object> property) {
 		properties.add(property);
+		return this;
+	}
+
+	public PropertySet<T> add(PropertySet<? super T> properties) {
+		for (Function<? super T, Object> property : properties.properties)
+			this.properties.add(property);
 		return this;
 	}
 
@@ -42,6 +54,9 @@ public class PropertySet<T> {
 		if (!objectClass.isInstance(object))
 			return false;
 
+		if (hashCode != null && !hashCode.equals(object.hashCode()))
+			return false;
+
 		@SuppressWarnings("unchecked")
 		T thatObject = (T) object;
 		return properties.stream().allMatch(
@@ -49,8 +64,11 @@ public class PropertySet<T> {
 	}
 
 	public int generateHashCode() {
-		return properties.stream().map(p -> Objects.hashCode(p.apply(object)))
-				.reduce(0, (a, b) -> a ^ b);
+		if (hashCode != null)
+			return hashCode;
+		else
+			return properties.stream().map(p -> Objects.hashCode(p.apply(object)))
+					.reduce(0, (a, b) -> a ^ b);
 	}
 
 	public List<Object> values() {
