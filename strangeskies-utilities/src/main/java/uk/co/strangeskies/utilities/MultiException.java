fa1632f3c286d323641f06b60c2ce07bedcd07f6
eliasvasylenko <eliasvasylenko@gmail.com>
2014-09-22 23:57:40 +0100

    Some attempts to improve MultiException.

--- strangeskies-utilities/src/main/java/uk/co/strangeskies/utilities/MultiException.java
+++ strangeskies-utilities/src/main/java/uk/co/strangeskies/utilities/MultiException.java
@@ -3,7 +3,9 @@ package uk.co.strangeskies.utilities;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class MultiException extends RuntimeException {
@@ -11,13 +13,32 @@ public class MultiException extends RuntimeException {
 
 	private final List<Throwable> causes;
 
-	public MultiException(Collection<? extends Throwable> causes) {
-		this.causes = new ArrayList<>(causes);
-	}
-
 	public MultiException(String message, Collection<? extends Throwable> causes) {
 		super(message, causes.stream().findFirst().get());
 		this.causes = new ArrayList<>(causes);
+	}
+
+	public MultiException(Collection<? extends Throwable> causes) {
+		super(causes.stream().findFirst().get());
+		this.causes = new ArrayList<>(causes);
+	}
+
+	public MultiException(String message, Throwable... causes) {
+		this(Arrays.asList(causes));
+	}
+
+	public MultiException(Throwable... causes) {
+		this(Arrays.asList(causes));
+	}
+
+	public MultiException(String message) {
+		super(message);
+		causes = Collections.emptyList();
+	}
+
+	public MultiException() {
+		super();
+		causes = Collections.emptyList();
 	}
 
 	@Override
@@ -33,10 +54,9 @@ public class MultiException extends RuntimeException {
 	@Override
 	public void printStackTrace(PrintWriter output) {
 		super.printStackTrace(output);
-		int i = 0;
-		for (Throwable cause : causes) {
-			output.println("Caused by (" + ++i + " of " + causes.size() + "):");
-			cause.printStackTrace(output);
+		for (int i = 2; i <= causes.size(); i++) {
+			output.println("Caused by (" + i + " of " + causes.size() + "):");
+			causes.get(i).printStackTrace(output);
 			output.flush();
 		}
 	}
