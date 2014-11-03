59bd1d3ccb8f348d79e2c33c2ec3d7452cec870a
eliasvasylenko <eliasvasylenko@gmail.com>
2014-11-03 23:26:20 +0000

    Reminder that shape configurator API should not be considered complete...

--- strangeskies-mathematics-geometry/src/main/java/uk/co/strangeskies/mathematics/geometry/shape/PolygonConfigurator.java
+++ strangeskies-mathematics-geometry/src/main/java/uk/co/strangeskies/mathematics/geometry/shape/PolygonConfigurator.java
@@ -2,6 +2,9 @@ package uk.co.strangeskies.mathematics.geometry.shape;
 
 import uk.co.strangeskies.mathematics.values.Value;
 
+/*
+ * TODO specify winding rule where appropriate
+ */
 public interface PolygonConfigurator<V extends Value<V>> {
 	CompoundPolygon<?, V> compound();
 
