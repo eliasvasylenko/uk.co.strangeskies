2205d76a93b9318ab732e213bd727d6e30bc4a3a
Elias N Vasylenko <eliasvasylenko@gmail.com>
2015-04-14 11:12:57 +0100

    cleared everything from pages...

--- uk.co.strangeskies.expressions/src/uk/co/strangeskies/mathematics/expression/MutableExpression.java
+++
@@ -1,7 +0,0 @@
-package uk.co.strangeskies.mathematics.expression;
-
-import java.util.concurrent.locks.Lock;
-
-public interface MutableExpression<T> extends Expression<T> {
-	public Lock getWriteLock();
-}
