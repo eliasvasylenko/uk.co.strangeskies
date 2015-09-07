541da7225da65d71f17d5515e5ba1d5b921f5bef
Elias N Vasylenko <eliasvasylenko@gmail.com>
2015-09-07 09:18:00 +0100

    documentation for Expression

--- uk.co.strangeskies.expressions/src/uk/co/strangeskies/mathematics/expression/Expression.java
+++ uk.co.strangeskies.expressions/src/uk/co/strangeskies/mathematics/expression/Expression.java
@@ -44,38 +44,29 @@ import uk.co.strangeskies.utilities.Observer;
  */
 public interface Expression<T> extends Observable<Expression<T>> {
 	/**
-	 * <p>
-	 * This should always return the correct value for this Expression. Be careful
-	 * to remember that the object returned from getValue() should not be an
-	 * updatable reference, i.e. it should be either an immutable class or a const
-	 * reference. This is important, but it does not mean that the return value
-	 * can necessarily be relied upon not to change when this expression is
-	 * updated.
+	 * This should always return the correct current value for this Expression. Be
+	 * careful to remember that the reference returned should not be able to
+	 * mutate this {@link Expression}, i.e. it should be either an immutable
+	 * class, a const reference, or a copy of the underlying value. This is
+	 * important, but conversely it does <em>not</em> mean that the return value
+	 * can necessarily be relied upon not to not mutate when this expression is
+	 * updated, unless a read lock is held.
 	 *
 	 * <p>
 	 * Once a value has been returned, it is up to the implementing Expression as
 	 * to whether the value will be reliable such that it will remain the same
 	 * even if the conceptual value of this expression subsequently changes, or
 	 * whether it will update automatically with the expression. Please only rely
-	 * on either behaviour if it is explicitly documented, otherwise copy the
-	 * returned value if you need a persistent reference which is safe to update
-	 * and safe from updates, or re-fetch the result when you need the updated
-	 * value.
-	 * 
-	 * <p>
-	 * Neither behaviour is explicitly required in preference to the other as much
-	 * of the time it could be very wasteful to require a value be copied, but at
-	 * the same time, it could often be impossible to return a persistently linked
-	 * value, e.g. for the case of a value type which is an immutable class, such
-	 * as String.
+	 * on either behaviour if it is explicitly documented, otherwise use
+	 * {@link #decoupleValue()} if you need a persistent reference which is safe
+	 * <em>to</em> mutate and/or safe <em>from</em> external mutation.
 	 * 
 	 * <p>
 	 * The observers should only ever be notified of an update from the thread
 	 * which has the write lock on an {@link Expression}, and {@link Expression}s
 	 * should be careful to only notify observers when they are in a state where
-	 * their value can be fetched. This is discouraged, though. Expressions should
-	 * generally update lazily, not eagerly.
-	 * 
+	 * their value can be fetched. Immediate fetch is discouraged, though.
+	 * Expressions should generally update lazily, not eagerly.
 	 * 
 	 * @return The fully evaluated value of this Expression at the time of method
 	 *         invocation.
