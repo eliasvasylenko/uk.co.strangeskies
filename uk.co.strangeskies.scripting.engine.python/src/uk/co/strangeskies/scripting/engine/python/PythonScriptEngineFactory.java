640330e6c7bd98245dfeef2b3378614c8da488dd
Elias N Vasylenko <eliasvasylenko@gmail.com>
2016-11-02 20:39:18 +0000

    got rid of somewhat unhelpful ${ss;} macro

--- uk.co.strangeskies.scripting.engine.python/src/uk/co/strangeskies/scripting/engine/python/PythonScriptEngineFactory.java
+++ uk.co.strangeskies.scripting.engine.python/src/uk/co/strangeskies/scripting/engine/python/PythonScriptEngineFactory.java
@@ -38,7 +38,6 @@ import org.python.jsr223.PyScriptEngineFactory;
 public class PythonScriptEngineFactory extends PyScriptEngineFactory {
 	public PythonScriptEngineFactory() {
 		System.setProperty("python.console.encoding", "UTF-8");
-		System.setProperty("python.home", "/home/eli/Downloads/jython-standalone-2.7.0/Lib");
 		System.setProperty("python.import.site", "false");
 	}
 }
