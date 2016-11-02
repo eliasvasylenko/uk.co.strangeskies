a2c34c56b64263efbcd75bf9c684d56e25338687
Elias N Vasylenko <eliasvasylenko@gmail.com>
2016-11-02 16:48:24 +0000

    license text formatting

--- uk.co.strangeskies.scripting.engine.kotlin/src/com/intellij/openapi/application/PathManager.java
+++ uk.co.strangeskies.scripting.engine.kotlin/src/com/intellij/openapi/application/PathManager.java
@@ -1,17 +1,34 @@
 /*
- * Copyright 2000-2016 JetBrains s.r.o.
+ * Copyright (C) 2016 Elias N Vasylenko <eliasvasylenko@strangeskies.co.uk>
+ *      __   _______  ____           _       __     _      __       __
+ *    ,`_ `,|__   __||  _ `.        / \     |  \   | |  ,-`__`¬  ,-`__`¬
+ *   ( (_`-'   | |   | | ) |       / . \    | . \  | | / .`  `' / .`  `'
+ *    `._ `.   | |   | |<. L      / / \ \   | |\ \ | || |    _ | '--.
+ *   _   `. \  | |   | |  `.`.   / /   \ \  | | \ \| || |   | || +--'
+ *  \ \__.' /  | |   | |    \ \ / /     \ \ | |  \ ` | \ `._' | \ `.__,.
+ *   `.__.-`   |_|   |_|    |_|/_/       \_\|_|   \__|  `-.__.J  `-.__.J
+ *                   __    _         _      __      __
+ *                 ,`_ `, | |  _    | |  ,-`__`¬  ,`_ `,
+ *                ( (_`-' | | ) |   | | / .`  `' ( (_`-'
+ *                 `._ `. | L-' L   | || '--.     `._ `.
+ *                _   `. \| ,.-^.`. | || +--'    _   `. \
+ *               \ \__.' /| |    \ \| | \ `.__,.\ \__.' /
+ *                `.__.-` |_|    |_||_|  `-.__.J `.__.-`
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This file is part of uk.co.strangeskies.scripting.engine.kotlin.
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * uk.co.strangeskies.scripting.engine.kotlin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * uk.co.strangeskies.scripting.engine.kotlin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package com.intellij.openapi.application;
 
