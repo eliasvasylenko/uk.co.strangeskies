a75976e62c2538a16b2bda34a80f83cc6842207b
Elias N Vasylenko <eliasvasylenko@gmail.com>
2017-01-04 11:05:53 +0000

    Updated license text copyright dates. Happy new year!

--- uk.co.strangeskies.reflection.resource/src/uk/co/strangeskies/reflection/resource/package-info.java
+++ uk.co.strangeskies.reflection.resource/src/uk/co/strangeskies/reflection/resource/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Elias N Vasylenko <eliasvasylenko@strangeskies.co.uk>
+ * Copyright (C) 2017 Elias N Vasylenko <eliasvasylenko@strangeskies.co.uk>
  *      __   _______  ____           _       __     _      __       __
  *    ,`_ `,|__   __||  _ `.        / \     |  \   | |  ,-`__`¬  ,-`__`¬
  *   ( (_`-'   | |   | | ) |       / . \    | . \  | | / .`  `' / .`  `'
@@ -15,14 +15,14 @@
  *               \ \__.' /| |    \ \| | \ `.__,.\ \__.' /
  *                `.__.-` |_|    |_||_|  `-.__.J `.__.-`
  *
- * This file is part of uk.co.strangeskies.reflection.
+ * This file is part of uk.co.strangeskies.reflection.resource.
  *
- * uk.co.strangeskies.reflection is free software: you can redistribute it and/or modify
+ * uk.co.strangeskies.reflection.resource is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * uk.co.strangeskies.reflection is distributed in the hope that it will be useful,
+ * uk.co.strangeskies.reflection.resource is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
