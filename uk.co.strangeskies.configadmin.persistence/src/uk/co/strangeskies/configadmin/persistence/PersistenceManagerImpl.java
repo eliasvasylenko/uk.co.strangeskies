30a3d979cbf611b2236dc906a14f0bb8793f076b
Elias N Vasylenko <eliasvasylenko@gmail.com>
2017-03-19 21:46:34 +0000

    license text update

--- uk.co.strangeskies.configadmin.persistence/src/uk/co/strangeskies/configadmin/persistence/PersistenceManagerImpl.java
+++ uk.co.strangeskies.configadmin.persistence/src/uk/co/strangeskies/configadmin/persistence/PersistenceManagerImpl.java
@@ -1,29 +1,34 @@
 /*
- * Copyright (C) 2017 Scientific Analysis Instruments Limited <contact@saiman.co.uk>
- *          ______         ___      ___________
- *       ,'========\     ,'===\    /========== \
- *      /== \___/== \  ,'==.== \   \__/== \___\/
- *     /==_/____\__\/,'==__|== |     /==  /
- *     \========`. ,'========= |    /==  /
- *   ___`-___)== ,'== \____|== |   /==  /
- *  /== \__.-==,'==  ,'    |== '__/==  /_
- *  \======== /==  ,'      |== ========= \
- *   \_____\.-\__\/        \__\\________\/
+ * Copyright (C) 2017 Elias N Vasylenko <eliasvasylenko@strangeskies.co.uk>
+ *      __   _______  ____           _       __     _      __       __
+ *    ,`_ `,|__   __||  _ `.        / \     |  \   | |  ,-`__`¬  ,-`__`¬
+ *   ( (_`-'   | |   | | ) |       / . \    | . \  | | / .`  `' / .`  `'
+ *    `._ `.   | |   | |<. L      / / \ \   | |\ \ | || |    _ | '--.
+ *   _   `. \  | |   | |  `.`.   / /   \ \  | | \ \| || |   | || +--'
+ *  \ \__.' /  | |   | |    \ \ / /     \ \ | |  \ ` | \ `._' | \ `.__,.
+ *   `.__.-`   |_|   |_|    |_|/_/       \_\|_|   \__|  `-.__.J  `-.__.J
+ *                   __    _         _      __      __
+ *                 ,`_ `, | |  _    | |  ,-`__`¬  ,`_ `,
+ *                ( (_`-' | | ) |   | | / .`  `' ( (_`-'
+ *                 `._ `. | L-' L   | || '--.     `._ `.
+ *                _   `. \| ,.-^.`. | || +--'    _   `. \
+ *               \ \__.' /| |    \ \| | \ `.__,.\ \__.' /
+ *                `.__.-` |_|    |_||_|  `-.__.J `.__.-`
  *
- * This file is part of uk.co.saiman.msapex.
+ * This file is part of uk.co.strangeskies.configadmin.persistence.
  *
- * uk.co.saiman.msapex is free software: you can redistribute it and/or modify
+ * uk.co.strangeskies.configadmin.persistence is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * uk.co.saiman.msapex is distributed in the hope that it will be useful,
+ * uk.co.strangeskies.configadmin.persistence is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package uk.co.strangeskies.configadmin.persistence;
 
