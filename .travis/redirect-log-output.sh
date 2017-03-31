a878df8e8826b94bcbb8e54509b4d603c4a98722
Elias N Vasylenko <eliasvasylenko@gmail.com>
2017-03-31 17:30:31 +0100

    more attempts to redirect output properly

--- .travis/redirect-log-output.sh
+++ .travis/redirect-log-output.sh
@@ -11,16 +11,7 @@ dump_output() {
    echo Tailing the last 500 lines of output:
    tail -500 $BUILD_OUTPUT  
 }
-error_handler() {
-  echo ERROR: An error was encountered with the build.
-  dump_output
-  kill $PING_LOOP_PID
-  exit 1
-}
-# If an error occurs, run our error handler to output a tail of the build
-trap 'error_handler' ERR
 
 # Set up a repeating loop to send some output to Travis.
-
 bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
 PING_LOOP_PID=$!
