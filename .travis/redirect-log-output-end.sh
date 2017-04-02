654483c401af362fdbc9f552a28e5ad6710b982e
Elias N Vasylenko <eliasvasylenko@gmail.com>
2017-04-02 20:31:42 +0100

    simplified .travis.yaml

--- .travis/redirect-log-output-end.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Abort on Error
-set -e
-
-# The build finished without returning an error so dump a tail of the output
-dump_output
-
-# nicely terminate the ping output loop
-kill $PING_LOOP_PID
