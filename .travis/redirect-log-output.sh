28496fa56141c8869da56b9b7ed1a4f3ad0a00a4
Elias N Vasylenko <eliasvasylenko@gmail.com>
2017-04-02 21:31:52 +0100

    redirect log output for more caching testing

---
+++ .travis/redirect-log-output.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Abort on Error
+set -e
+
+export PING_SLEEP=30s
+export BUILD_OUTPUT=$TRAVIS_BUILD_DIR/.travis/build.out
+
+touch $BUILD_OUTPUT
+
+dump_output() {
+  echo Tailing the last 500 lines of output:
+  tail -500 $BUILD_OUTPUT  
+}
+
+# Set up a repeating loop to send some output to Travis.
+bash -c "while true; do echo \$(date) - building ...; sleep $PING_SLEEP; done" &
+PING_LOOP_PID=$!
+
