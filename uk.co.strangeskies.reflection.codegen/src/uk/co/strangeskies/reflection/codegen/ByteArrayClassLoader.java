d5e0b8fa58dfbd436f8af02e7ce95ad4263f2063
Elias N Vasylenko <eliasvasylenko@gmail.com>
2017-05-20 16:56:56 +0100

    amalgamated InjectionClassLoader and ByteArrayClassLoader
    now bytecode can be loaded directly, or if there is a chance of circular
    dependencies we can provide a set of bytecodes at once and defer loading
    until use.

--- uk.co.strangeskies.reflection.codegen/src/uk/co/strangeskies/reflection/codegen/ByteArrayClassLoader.java
+++ uk.co.strangeskies.reflection.codegen/src/uk/co/strangeskies/reflection/codegen/ByteArrayClassLoader.java
@@ -1,11 +1,54 @@
 package uk.co.strangeskies.reflection.codegen;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
 public class ByteArrayClassLoader extends ClassLoader {
+	private final Map<String, byte[]> injectedClasses = new HashMap<>();
+
 	public ByteArrayClassLoader(ClassLoader parent) {
 		super(parent);
 	}
 
-	public Class<?> injectClass(String name, byte[] bytecode) {
-		return defineClass(name, bytecode, 0, bytecode.length);
+	@Override
+	protected Class<?> findClass(String name) throws ClassNotFoundException {
+		if (injectedClasses.containsKey(name)) {
+			byte[] bytecode = injectedClasses.get(name);
+			return defineClass(name, bytecode, 0, bytecode.length);
+		} else {
+			return super.findClass(name);
+		}
+	}
+
+	public ByteArrayClassLoader addClass(String name, byte[] bytecode) {
+		injectedClasses.put(name, bytecode);
+		return this;
+	}
+
+	public ByteArrayClassLoader addClasses(Map<String, byte[]> bytecodes) {
+		bytecodes.entrySet().stream().forEach(e -> addClass(e.getKey(), e.getValue()));
+		return this;
+	}
+
+	public Class<?> defineClass(String name, byte[] bytecode) {
+		Class<?> injectedClass = defineClass(name, bytecode, 0, bytecode.length);
+		injectedClasses.put(name, bytecode);
+		return injectedClass;
+	}
+
+	public ByteArrayClassLoader defineClasses(Map<String, byte[]> bytecodes) {
+		bytecodes.entrySet().stream().forEach(e -> defineClass(e.getKey(), e.getValue()));
+		return this;
+	}
+
+	public Stream<String> getInjectedClasses() {
+		return injectedClasses.keySet().stream();
+	}
+
+	public byte[] getInjectedBytes(String name) {
+		if (!injectedClasses.containsKey(name))
+			throw new IllegalArgumentException();
+		return injectedClasses.get(name);
 	}
 }
