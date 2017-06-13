602b912ca840ed3c66e1c0a502ced676f42b493f
Elias N Vasylenko <eliasvasylenko@gmail.com>
2017-06-13 18:40:29 +0100

    updated license texts

--- uk.co.strangeskies.utilities/src/uk/co/strangeskies/reflection/classloading/FilteringClassLoader.java
+++ uk.co.strangeskies.utilities/src/uk/co/strangeskies/reflection/classloading/FilteringClassLoader.java
@@ -15,14 +15,14 @@
  *               \ \__.' /| |    \ \| | \ `.__,.\ \__.' /
  *                `.__.-` |_|    |_||_|  `-.__.J `.__.-`
  *
- * This file is part of uk.co.strangeskies.reflection.resource.
+ * This file is part of uk.co.strangeskies.utilities.
  *
- * uk.co.strangeskies.reflection.resource is free software: you can redistribute it and/or modify
+ * uk.co.strangeskies.utilities is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * uk.co.strangeskies.reflection.resource is distributed in the hope that it will be useful,
+ * uk.co.strangeskies.utilities is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
