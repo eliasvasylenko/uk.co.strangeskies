3329770bf45aa3bcb53b0fe833449de8ff343e4e
Elias N Vasylenko <eliasvasylenko@gmail.com>
2017-08-29 08:42:08 +0100

    variable matcher builder methods properly generically typed

--- uk.co.strangeskies.reflection.token/src/uk/co/strangeskies/reflection/token/VariableMatcher.java
+++ uk.co.strangeskies.reflection.token/src/uk/co/strangeskies/reflection/token/VariableMatcher.java
@@ -46,79 +46,87 @@ import java.util.function.Predicate;
 import uk.co.strangeskies.reflection.Visibility;
 
 public class VariableMatcher<O, T> implements Predicate<FieldToken<?, ?>> {
-	public static VariableMatcher<Object, Object> anyVariable() {
-		return new VariableMatcher<>(empty(), empty(), empty(), empty());
-	}
+  public static VariableMatcher<Object, Object> anyVariable() {
+    return new VariableMatcher<>(empty(), empty(), empty(), empty());
+  }
 
-	private final Optional<String> name;
-	private final Optional<Visibility> visibility;
-	private final Optional<TypeToken<?>> assignableTo;
-	private final Optional<TypeToken<?>> assignableFrom;
+  private final Optional<String> name;
+  private final Optional<Visibility> visibility;
+  private final Optional<TypeToken<?>> assignableTo;
+  private final Optional<TypeToken<?>> assignableFrom;
 
-	protected VariableMatcher(
-			Optional<String> name,
-			Optional<Visibility> visibility,
-			Optional<TypeToken<?>> assignableTo,
-			Optional<TypeToken<?>> assignableFrom) {
-		this.name = name;
-		this.visibility = visibility;
-		this.assignableTo = assignableTo;
-		this.assignableFrom = assignableFrom;
-	}
+  protected VariableMatcher(
+      Optional<String> name,
+      Optional<Visibility> visibility,
+      Optional<TypeToken<?>> assignableTo,
+      Optional<TypeToken<?>> assignableFrom) {
+    this.name = name;
+    this.visibility = visibility;
+    this.assignableTo = assignableTo;
+    this.assignableFrom = assignableFrom;
+  }
 
-	@SuppressWarnings("unchecked")
-	public Optional<FieldToken<O, T>> match(FieldToken<?, ?> field) {
-		if (test(field))
-			return of((FieldToken<O, T>) field);
-		else
-			return empty();
-	}
+  @SuppressWarnings("unchecked")
+  public Optional<FieldToken<O, T>> match(FieldToken<?, ?> field) {
+    if (test(field))
+      return of((FieldToken<O, T>) field);
+    else
+      return empty();
+  }
 
-	@Override
-	public boolean test(FieldToken<?, ?> field) {
-		return testImpl(field.getName(), field.getVisibility(), field.getFieldType().getType());
-	}
+  @Override
+  public boolean test(FieldToken<?, ?> field) {
+    return testImpl(field.getName(), field.getVisibility(), field.getFieldType().getType());
+  }
 
-	public boolean test(Field field) {
-		return testImpl(field.getName(), forModifiers(field.getModifiers()), field.getGenericType());
-	}
+  public boolean test(Field field) {
+    return testImpl(field.getName(), forModifiers(field.getModifiers()), field.getGenericType());
+  }
 
-	private boolean testImpl(String name, Visibility visibility, Type type) {
-		return this.name.map(name::equals).orElse(true)
-				&& this.visibility.filter(visibility::equals).isPresent()
-				&& this.assignableTo.filter(t -> t.satisfiesConstraintFrom(SUBTYPE, type)).isPresent()
-				&& this.assignableFrom.filter(t -> t.satisfiesConstraintTo(SUBTYPE, type)).isPresent();
-	}
+  private boolean testImpl(String name, Visibility visibility, Type type) {
+    return this.name.map(name::equals).orElse(true)
+        && this.visibility.filter(visibility::equals).isPresent()
+        && this.assignableTo.filter(t -> t.satisfiesConstraintFrom(SUBTYPE, type)).isPresent()
+        && this.assignableFrom.filter(t -> t.satisfiesConstraintTo(SUBTYPE, type)).isPresent();
+  }
 
-	public VariableMatcher<O, T> named(String name) {
-		return new VariableMatcher<>(of(name), visibility, assignableTo, assignableFrom);
-	}
+  public VariableMatcher<O, T> named(String name) {
+    return new VariableMatcher<>(of(name), visibility, assignableTo, assignableFrom);
+  }
 
-	public VariableMatcher<O, T> visibleTo(Visibility visibility) {
-		return new VariableMatcher<>(name, of(visibility), assignableTo, assignableFrom);
-	}
+  public VariableMatcher<O, T> visibleTo(Visibility visibility) {
+    return new VariableMatcher<>(name, of(visibility), assignableTo, assignableFrom);
+  }
 
-	public VariableMatcher<O, T> typed(TypeToken<T> type) {
-		return new VariableMatcher<>(name, visibility, of(type), of(type));
-	}
+  public <U> VariableMatcher<O, U> typed(TypeToken<U> type) {
+    return new VariableMatcher<>(name, visibility, of(type), of(type));
+  }
 
-	public VariableMatcher<O, T> typed(Class<T> type) {
-		return typed(forClass(type));
-	}
+  public <U> VariableMatcher<O, U> typed(Class<U> type) {
+    return typed(forClass(type));
+  }
 
-	public VariableMatcher<O, T> assignableTo(TypeToken<T> type) {
-		return new VariableMatcher<>(name, visibility, of(type), assignableFrom);
-	}
+  public <U> VariableMatcher<O, U> assignableTo(TypeToken<U> type) {
+    return new VariableMatcher<>(name, visibility, of(type), assignableFrom);
+  }
 
-	public VariableMatcher<O, T> assignableTo(Class<T> type) {
-		return assignableTo(forClass(type));
-	}
+  public <U> VariableMatcher<O, U> assignableTo(Class<U> type) {
+    return assignableTo(forClass(type));
+  }
 
-	public VariableMatcher<O, T> assignableFrom(TypeToken<T> type) {
-		return new VariableMatcher<>(name, visibility, assignableTo, of(type));
-	}
+  public <U> VariableMatcher<O, U> assignableFrom(TypeToken<U> type) {
+    return new VariableMatcher<>(name, visibility, assignableTo, of(type));
+  }
 
-	public VariableMatcher<O, T> assignableFrom(Class<T> type) {
-		return assignableFrom(forClass(type));
-	}
+  public <U> VariableMatcher<O, U> assignableFrom(Class<U> type) {
+    return assignableFrom(forClass(type));
+  }
+
+  public <U> MethodMatcher<U, T> receiving(TypeToken<U> type) {
+    return null;
+  }
+
+  public <U> MethodMatcher<U, T> receiving(Class<U> type) {
+    return receiving(forClass(type));
+  }
 }
